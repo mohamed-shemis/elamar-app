502dcaa9f447914c7109f2310fa0434573def901
mohamed-shemis <mohamed_salamashemis@yahoo.com>
2026-05-16 13:09:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280, height: 820, minWidth: 400, minHeight: 600,
+    title: 'ELAMAR – برنامج إدارة المعدات',
+    icon: path.join(__dirname, 'src', 'icon.png'),
+    webPreferences: { nodeIntegration: false, contextIsolation: true, webSecurity: false },
+    backgroundColor: '#0f172a', autoHideMenuBar: true,
+  });
+  Menu.setApplicationMenu(null);
+  win.loadFile(path.join(__dirname, 'src', 'index.html'));
+}
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
+});
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
