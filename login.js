aecb8704f946685968f5fcfb344fa871389fa744
mohamed-shemis <mohamed_salamashemis@yahoo.com>
2026-06-29 13:51:52 +0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,17 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+document.getElementById("loginBtn").onclick = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+
+    // بعد النجاح
+    window.location.href = "dashboard.html";
+
+  } catch (e) {
+    alert("Login failed");
+  }
+};
