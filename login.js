2e98796b42034d9566359070ad9c37577b445854
mohamed-shemis <mohamed_salamashemis@yahoo.com>
2026-06-29 13:57:27 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,17 +1,35 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 document.getElementById("loginBtn").onclick = async () => {
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
-    await signInWithEmailAndPassword(auth, email, password);
 
-    // بعد النجاح
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    // 👇 مهم جداً: نجيب role من Firestore
+    const userSnap = await getDoc(doc(db, "users", user.uid));
+
+    if (!userSnap.exists()) {
+      alert("User not found in database");
+      return;
+    }
+
+    const role = userSnap.data().role;
+
+    // نخزن role في localStorage
+    localStorage.setItem("role", role);
+
+    // تحويل
     window.location.href = "dashboard.html";
 
   } catch (e) {
     alert("Login failed");
+    console.log(e);
   }
 };
