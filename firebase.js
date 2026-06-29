76d0edacc55d937600a7a226ba1400b391068df7
mohamed-shemis <mohamed_salamashemis@yahoo.com>
2026-06-29 13:49:15 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "PUT_HERE",
-  authDomain: "PUT_HERE",
-  projectId: "PUT_HERE",
-  storageBucket: "PUT_HERE",
-  messagingSenderId: "PUT_HERE",
-  appId: "PUT_HERE"
+  apiKey: "AIzaSyDWN6XcR4pVu4KO7YHWKvSC3F3oMdfPd30",
+  authDomain: "elamar-erp.firebaseapp.com",
+  projectId: "elamar-erp",
+  storageBucket: "elamar-erp.firebasestorage.app",
+  messagingSenderId: "802572455002",
+  appId: "1:802572455002:web:0d67fecc8024127f889c63"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
